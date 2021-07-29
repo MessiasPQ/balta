e9de6a1d3247055860810c176a3f07b96d0b83f4
Messias Queiroz <messias1572@hotmail.com>
2021-07-28 22:31:10 -0300

    Modelando Article e outros pequenos ajustes

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Balta.ContentContext;
 
 namespace Balta
@@ -7,9 +8,20 @@ namespace Balta
     {
         static void Main(string[] args)
         {
-            var course = new Course();
-            course.Level = ContentContext.Enums.EContentLevel.Beginner;
-            System.Console.WriteLine("Olá Mundo!");
+            var articles = new List<Article>();
+            articles.Add(new Article("Artigo sobre POO", "Programação orientada a objetos"));
+            articles.Add(new Article("Artigo sobre C#", "Linguagem de programação"));
+            articles.Add(new Article("Artigo sobre .NET", "Framework"));
+
+
+            foreach (var article in articles)
+            {
+                System.Console.WriteLine("-----------------------------------------");
+                System.Console.WriteLine(article.Title);
+                System.Console.WriteLine(article.Url);
+                System.Console.WriteLine(article.Id);
+            }
+
         }
     }
 }
